194c0d9c6dd9ceee662ed50714f85fb501b4e2c1
Owonubi Job Sunday <jobowonubi@gmail.com>
2022-02-05 17:59:14 +0100

    Logout, Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,4 +2,26 @@
 
 @section('content')
     <h1>Login</h1>
+    <form method="POST" action="{{ route('login')  }}">
+        @csrf
+
+        <div class="form-group mb-3">
+            <label for="email">Email address</label>
+            <input type="email"  value="{{ old('email')  }}"  class = "form-control"  name='email' id="email" aria-describedby="email" placeholder="Enter email">
+
+        </div>
+        <div class="form-group mb-3">
+            <label for="password">Password</label>
+            <input type="password"  class = "form-control"  aria-describedby="password" name="password" id="password" placeholder="Password">
+
+        </div>
+        @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li class="text-danger">{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 @endsection
--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -37,6 +37,8 @@
                     <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                         @auth
                             <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                            <a href="#" onclick="document.getElementById('logout').submit();" class="text-sm text-gray-700 dark:text-gray-500 underline">Log out</a>
+                            <form method="POST" id="logout" action="{{ url('/logout') }}" style="display: none">@csrf</form>
                         @else
                             <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
